b2079b77d53c32e099f1112f8a087fbbf8129212
大東圭輔 <is0784rf@ed.ritsumei.ac.jp>
2026-06-29 01:39:31 +0900

    cliのエントリポイントを修正

--- src/share/cli.py
+++ src/share/cli.py
@@ -4,12 +4,7 @@ import argparse
 import logging
 import sys
 
-main
-from share.upload import add_upload_parser
-from share.create import add_create_parser
-
-from share import auth, upload
-main
+from share import auth, upload, create
 
 
 def build_parser() -> argparse.ArgumentParser:
@@ -17,16 +12,10 @@ def build_parser() -> argparse.ArgumentParser:
     parser.add_argument("-v", "--verbose", action="store_true", help="詳細なログを出力する")
     sub = parser.add_subparsers(dest="command", required=True)
 
- main
-    # TODO(担当者): 各サブコマンドを追加し、set_defaults(func=...) でハンドラを紐づける
-    # 例) sub.add_parser("auth", help="Dropbox 認証を行う").set_defaults(func=cmd_auth)
-    add_upload_parser(sub)  # アップロード（Nakatani）
-    add_create_parser(sub)  # 共有リンクの発行（Daito）
-
     auth.add_auth_parser(sub)
-    upload.add_upload_parser(sub)  # 実装済み
+    upload.add_upload_parser(sub)
+    create.add_create_parser(sub)
     # TODO: link, list, revoke のサブコマンドを追加
- main
 
     return parser
 
